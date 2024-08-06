5cce3e67cf921bb70b843295c16cfc70f4901016
Sirigiri Rakesh <94678816+Rakesh-sirigiri@users.noreply.github.com>
2024-08-06 12:43:12 +0530

    Add files via upload

---
+++ Remove a key in dictionary.py
@@ -0,0 +1,5 @@
+test_dict = {"Arushi": 22, "Mani":21, "Haritha": 21}
+print("The dictionary before performing remove is :", test_dict)
+del test_dict['Mani']
+print("The dictionary after remove is :", test_dict)
+
